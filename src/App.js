302c3ee06cb3bf8be85d1af67287479a59f02e11
Brook07 <utsavadhr@gmail.com>
2025-09-09 08:55:31 +0545

    initial setup commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react';
+import "./App.css";
+
+
+function App() {
+    const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+    return (
+    <div className="App">
+      <h1>My Todo Application</h1>
+
+      <div classname='todo-wrapper'>
+        <div classname='todo-input'>
+
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="What's the task?" />
+          </div>
+
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the Description?" />
+          </div>
+
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className='btn-area'>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)} >Todo</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)} >Completed</button>
+        </div>
+        
+        <div className="todo-list">
+
+        <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
